4b62be9a3117e7051908bc2dbac6a697d857d2df
nsraymond <raymondnsiegbunam@gmail.com>
2023-07-21 15:31:42 +0100

    making it responsive

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
   return (
-    <div className="fixed w-full h-[60px] text-white flex justify-between items-center px-4 bg-[#0a192f]">
+    <div className="relative w-full h-[60px] text-white flex justify-between items-center px-4 bg-[#0a192f]">
       <div>
       <p className='text-2xl'> rCN </p>
       </div>
